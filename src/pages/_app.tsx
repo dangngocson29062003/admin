9366baf3d09531364f20987ff6cc2c2e2f5e940b
Luyen tap IT cua SonDang <dangngocson29062003@gmail.com>
2024-06-23 01:20:08 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
+"use client";
+import { Layout } from "antd";
 import type { AppProps } from "next/app";
-
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container p-4">
+      <Component {...pageProps} />
+    </div>
+  );
 }
